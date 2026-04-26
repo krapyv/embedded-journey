2757d2b4079c80e4a49abb0536d3e4fbec7e1c6b
ShadowDrake21 <dimka670020040@gmail.com>
2026-04-26 15:23:17 +0200

    electronics: kirchhoff's voltage law

---
+++ 01-c-fundamentals/day-2-double-pointers/findPairThatSumToTarget.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// Write a function that finds the first pair of elements that sum to a target value.
+// Return pointers to the two elements via double pointers.
+
+int find_pair
--- 01-c-fundamentals/electronicsTopics.md
+++ 01-c-fundamentals/electronicsTopics.md
@@ -1,3 +1,5 @@
 25.04: 
 The Ohm's Law: https://www.fluke.com/en/learn/blog/electrical/what-is-ohms-law
 
+26.04:
+Kirchhoff's Voltage Law (KVL): https://www.allaboutcircuits.com/textbook/direct-current/chpt-6/kirchhoffs-voltage-law-kvl/
