9940cb191b1a7cd0ce4bb3dbf1802bc5c1ea6d1f
ShadowDrake21 <dimka670020040@gmail.com>
2026-06-08 20:45:07 +0200

    feat: clock init function

--- 03-arm-bare-metal/day-8-adc-dma-sampling/sampling.c
+++ 03-arm-bare-metal/day-8-adc-dma-sampling/sampling.c
@@ -2,24 +2,43 @@
 #include "stm32f411.h"
 #include "sampling.h"
 
-void clocks_init(SAMP_HandleTypeDef *hsamp)
+void clocks_init(void)
 {
-    // ---- PORT CLOCK ENABLING
+// ---- PORT CLOCK ENABLING
 
-    // since only ports A and B can be ADC
-    if (hsamp->Port == GPIOA)
-    {
-        RCC->AHB1ENR |= (1 << 0);
-    }
-    else if (hsamp->Port == GPIOB)
-    {
-        RCC->AHB1ENR |= (1 << 1);
-    }
-    else
-    {
-        // incorrect port
-        return;
-    }
+// since only ports A and B can be ADC
+#if (TARGET_SAMP_PORT == HARDWARE_PORT_A)
+    RCC->AHB1ENR |= (1 << 0);
+
+#elif (TARGET_SAMP_PORT == HARDWARE_PORT_B)
+    RCC->AHB1ENR |= (1 << 1);
+#else
+#error "Invalid target port assignment! Must use HARDWARE_PORT_A or HARDWARE_PORT_B"
+#endif
+
+    // ---- DMA ENABLING ----
+
+    // DMA2 (bit 22) for reading ADC data
+    // DMA1 (bit 21) for USART2 communication
+
+    // 0x3 = 0b11
+    RCC->AHB1ENR |= 0x3 << 21;
+
+    // ---- ADC1 ENABLING ----
+    RCC->APB2ENR |= 1U << 8;
+
+    // ---- TIM2 ENABLING ----
+    RCC->APB1ENR |= 1U << 0;
+
+    // ---- UART2 ENABLING ----
+    RCC->APB1ENR |= 1U << 17;
+
+#if (TARGET_SAMP_PORT == HARDWARE_PORT_A)
+// Port A has already been enabled
+#elif (TARGET_SAMP_PORT == HARDWARE_PORT_B)
+    /* Port A enabling for USART */
+    RCC->AHB1ENR |= 1U << 0;
+#endif
 }
 
 void gpio_init()
--- 03-arm-bare-metal/day-8-adc-dma-sampling/sampling.h
+++ 03-arm-bare-metal/day-8-adc-dma-sampling/sampling.h
@@ -3,6 +3,16 @@
 
 #include "stm32f411.h"
 
+#define USART2_CLK_MANAGED_EXTERNALLY
+
+/* Plain integer tokens for the preprocessor */
+#define HARDWARE_PORT_A 0U
+#define HARDWARE_PORT_B 1U
+
+#define TARGET_SAMP_PORT HARDWARE_PORT_A
+
+#define SAMP_PORT GPIOA
+
 typedef struct
 {
     GPIO_RegDef_t *Port;
